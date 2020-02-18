b87ad85867350e9cee8b8afb4829975ae3424ca7
James <cordell.james@gmail.com>
2020-02-18 16:38:55 +0000

    add queries to build db

--- openSolarDbInsert.py
+++ openSolarDbInsert.py
@@ -5,6 +5,21 @@ import pytz
 import time
 
 
+#CREATE TABLE log (
+	#time INTEGER DEFAULT 0 NOT NULL,
+	#itemId INTEGER DEFAULT 0 NOT NULL,
+	#value NUMERIC DEFAULT 0 NOT NULL
+#);
+
+#CREATE INDEX log_time_IDX ON log (time,itemId);
+
+
+#CREATE TABLE status (
+	#"key" TEXT(255),
+	#value INTEGER
+#);
+
+
 class Db:
   conn = None
   cur = None
@@ -30,6 +45,6 @@ class Db:
     self.cur.execute(q)
    
 if __name__ == '__main__':
-    db = Db('OpenSolar')
+    db = Db('OpenSolar.db')
     print("INSERT INTO log (time,itemId,value) VALUES ("+str(int(time.time()))+",1,55)")
     print(db.query('INSERT INTO log (time,itemId,value) VALUES ('+str(int(time.time()))+',1,55)'))

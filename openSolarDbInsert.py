ec200007b09d83b2869ee837a2de45ba571d1efd
JamesCordell <cordell.james@gmail.com>
2020-02-24 17:15:35 +0000

    tidy and add w1thermsensor

--- openSolarDbInsert.py
+++ openSolarDbInsert.py
@@ -4,6 +4,18 @@ from datetime import datetime
 import pytz
 import time
 
+from w1thermsensor import W1ThermSensor
+try:
+  sensor = W1ThermSensor()
+except KernelModuleLoadError as e:
+  print(e)
+
+while True:
+  temp1 = sensor.get_temperature()
+  temp2 = sensor.get_temperature()
+  temp3 = sensor.get_temperature()
+  print("The temperature is %s celsius" % ((temp1 + temp2 + temp3) / 3))
+  time.sleep(1)
 
 #CREATE TABLE log (
 	#time INTEGER DEFAULT 0 NOT NULL,
@@ -19,22 +31,20 @@ import time
 	#value INTEGER
 #);
 
-
 class Db:
   conn = None
   cur = None
   def __init__(self,db_file):
     """ create a database connection to a SQLite database """
     try:
-        self.conn = sqlite3.connect(db_file, isolation_level=None)
-        print(sqlite3.version)
-        self.cur = self.conn.cursor()
+      self.conn = sqlite3.connect(db_file, isolation_level=None)
+      self.cur = self.conn.cursor()
     except Error as e:
-        print(e)
+      print(e)
     finally:
-        if not self.conn:
-            self.conn.close()
-            print('Closing db')
+      if not self.conn:
+        self.conn.close()
+        print('Closing db')
           
   def __del__(self):
     self.conn.commit()
@@ -46,5 +56,6 @@ class Db:
    
 if __name__ == '__main__':
     db = Db('OpenSolar.db')
-    print("INSERT INTO log (time,itemId,value) VALUES ("+str(int(time.time()))+",1,55)")
-    print(db.query('INSERT INTO log (time,itemId,value) VALUES ('+str(int(time.time()))+',1,55)'))
+    avgTemp = (sensor.get_temperature() + sensor.get_temperature() + sensor.get_temperature()) / 3
+    print("INSERT INTO log (time,itemId,value) VALUES ("+str(int(time.time()))+",1,"+avgTemp+")")
+    print(db.query('INSERT INTO log (time,itemId,value) VALUES ('+str(int(time.time()))+',1,'+avgTemp+')'))
